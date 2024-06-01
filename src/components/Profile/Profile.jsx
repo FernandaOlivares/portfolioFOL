5d7e90c6b5f5774e57c61cbb6a6e98637aa2a64c
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-05-31 21:45:36 -0400

    Location Updated

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Profile.module.css';
 import profilePicture from '../../assets/profilePicture.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 const Profile = () => (
   <div className={styles.profile}>
     <img src={profilePicture} alt="Profile" className={styles.photo} />
     <h2 className={styles.name}>Fernanda Olivares Lisperguer</h2>
     <h3 className={styles.title}>|| Full Stack Developer ||</h3>
+    <p className={styles.techStack}>
+    <FontAwesomeIcon icon={faMapMarkerAlt} /> Santiago - Chile</p>
     <p className={styles.techStack}>-</p>
     <p className={styles.techStack}>JavaScript | React | HTML | CSS | Redux | Node.js | SQL | Express.js | Git</p>
     <p className={styles.techStack}>English C2 Proficient | Native Spanish Speaker</p>
